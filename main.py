2c9029a2d03fd7212a8bb40345d920d6ead70821
Garieli <gabicristina015@gmail.com>
2024-10-13 21:23:36 -0300

    fix: atualizando RTC

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def main():
         st.write(f"A detecção ao vivo com {yolo_version} será exibida abaixo.")
 
         rtc_configuration = RTCConfiguration(
-            {"iceServers": [{"urls": ["stun:stun2.l.google.com:19302"]}]})
+            {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
         webrtc_ctx = webrtc_streamer(
             key="detector",
